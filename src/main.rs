137d175eb3bbb55836544014661d0ddbca488a12
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2025-06-14 21:30:05 +0800

    finalize kv store

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
 mod store;
+use store::Store;
 
 fn main() {
-    println!("Hello, world!");
+    let mut store = Store::new();
+
+    let key1 = "key1".to_string();
+    let val1 = "val1".to_string();
+
+    let key2 = "key2".to_string();
+    let val2 = "val2".to_string();
+
+    println!("Inserting - {}-{}, {}-{}", key1, val1, key2, val2);
+    store.set(&key1, &val1);
+    store.set(&key2, &val2);
+
+    println!("Getting key1");
+    if let Some(got_val) = store.get(&key1) {
+        println!("key: {}, val: {}", key1, got_val);
+    } else {
+        println!("key: {} not found", key1);
+    }
+
+    println!("Getting key2");
+    if let Some(got_val) = store.get(&key2) {
+        println!("key: {}, val: {}", key2, got_val);
+    } else {
+        println!("key: {} not found", key2);
+    }
+
+    println!("Deleting key2: {}", key2);
+    store.del(&key2);
+    println!("Getting key2 after deletion");
+    if let Some(got_val) = store.get(&key2) {
+        println!("key: {}, val: {}", key2, got_val);
+    } else {
+        println!("key: {} not found", key2);
+    }
 }
--- src/store.rs
+++ src/store.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+#[derive(Debug, Default, Clone)]
 pub struct Store {
     storage: HashMap<String, String>,
 }
@@ -11,16 +12,16 @@ impl Store {
         }
     }
 
-    pub fn get(&self, key: &str) -> Option<&String> {
-        self.storage.get(key)
+    pub fn get<K: AsRef<str>>(&self, key: K) -> Option<&String> {
+        self.storage.get(key.as_ref())
     }
 
-    pub fn set(&mut self, key: &String, value: &String) {
-        self.storage.insert(key.to_owned(), value.to_owned());
+    pub fn set<K: AsRef<str>, V: AsRef<str>>(&mut self, key: K, value: V) -> Option<String> {
+        self.storage.insert(key.as_ref().to_string(), value.as_ref().to_string())
     }
 
-    pub fn del(&mut self, key: &String) -> Option<String> {
-        self.storage.remove(key)
+    pub fn del<K: AsRef<str>>(&mut self, key: K) -> Option<String> {
+        self.storage.remove(key.as_ref())
     }
 }
 
@@ -31,10 +32,31 @@ mod tests {
     #[test]
     fn test_store_set_get_delete_and_get() {
         let mut store = Store::new();
-        let key = "the key".to_string();
-        let val = "the val".to_string();
+        let key = "the key";
+        let val = "the val";
         store.set(&key, &val);
 
-        assert_eq!(store.get(&key), Some(&val))
+        assert_eq!(store.get(key).map(|s| s.as_str()), Some(val));
+
+        store.del(&key);
+
+        assert_eq!(store.get(key).map(|s| s.as_str()), None);
+    }
+
+    #[test]
+    fn test_overwrite_set() {
+        let mut store = Store::new();
+
+        store.set("key1", "hello");
+        assert_eq!(store.get("key1").map(|s| s.as_str()), Some("hello"));
+
+        store.set("key1", "new value");
+        assert_eq!(store.get("key1").map(|s| s.as_str()), Some("new value"));
+    }
+
+    #[test]
+    fn test_del_with_non_existent_key() {
+        let mut store = Store::new();
+        assert_eq!(store.del("hello"), None);
     }
 }
